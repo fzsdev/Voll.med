03e99add61f09d9f1ae089eecb35106820459b1f
Felipe <fzs.fto@hotmail.com>
2023-05-02 20:06:01 -0300

    Entidades JPA

--- api/src/main/java/med/voll/api/controller/MedicosController.java
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicosController {
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMed dados) {
         System.out.println(dados);
