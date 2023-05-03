c046e0ebb39bb1f6054645152df0656c1fce13bf
Felipe <fzs.fto@hotmail.com>
2023-05-02 21:28:22 -0300

    Criar tabela Paciente, correção campo telefone e migration V3 do MySQL.

--- api/src/main/java/med/voll/api/controller/MedicosController.java
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,17 +1,29 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMed;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/medicos")
+@RequestMapping("medicos")
 public class MedicosController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMed dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMed dados) {
+        /*repository.save(new Medico(null, dados.nome(), dados.email(), dados.crm(), new Endereco(dados))); tem um metodo
+        * mais facil de receber os dados*/
+        repository.save(new Medico(dados));
     }
 }
