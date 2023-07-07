07f3844c21901290f5d25f2c4d4114662e95df98
Felipe <fzs.fto@hotmail.com>
2023-07-07 04:37:42 -0300

    Configuração auth0, mas com problemas de autenticação a serem resolvidos.

---
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,32 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.user.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    public String gerarToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(user.getLogin())
+                    .withClaim("id", user.getId())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
