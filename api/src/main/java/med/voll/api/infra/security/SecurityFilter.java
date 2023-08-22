d728c2ece1a54086f7773b15b0485f8e7e53f1e8
Felipe <fzs.fto@hotmail.com>
2023-08-22 15:02:18 -0300

    teste de controle de acesso + configuração do filter

--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,13 +19,23 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UserRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-
+        System.out.println("Inicio do filter!"); // mostrar a ordem de execução do filter, em securityConfig.class
         var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
 
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = repository.findByLogin(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("Login na requisição"); // final ordem de execução do filter em securityConfig.class
+        }
 
         //System.out.println(subject);
         filterChain.doFilter(request, response);
@@ -30,10 +43,9 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
-
-            throw new RuntimeException("Token JWT não enviado ao cabeçalho Authorization.");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
