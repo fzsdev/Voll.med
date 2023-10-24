2f88c1f483f0c8beb0155e9a4a6c546d720f879a
Felipe <fzs.fto@hotmail.com>
2023-10-24 17:48:02 -0300

    Funcionalidade cancelamento consultas +SQL

--- api/src/main/java/med/voll/api/domain/consulta/AgendaConsultas.java
+++ api/src/main/java/med/voll/api/domain/consulta/AgendaConsultas.java
@@ -29,8 +29,17 @@ public class AgendaConsultas {
 
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var medico = escolherMedico(dados);
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
